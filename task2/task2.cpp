0bbda12c33863b97e17f1562423c3df250b12d7a
pavbox <pavboxdev@gmail.com>
2017-12-12 01:30:55 +0300

    2y2s: add 2nd task

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -1,56 +1,137 @@
 
 /**
 
+	Даны целые чиста а1...an.
+	Если в данной последовательности ни одно четное число не расположено после нечетного,
+	то получить все отрицательные члены последовательности,
+	иначе – все положительные.
+
+	// если хотя бы одно четное число расположено после нечетного,
+	// т.е. после начала нечетных идут только нечетные, то вывести отрицательные
+
+	Порядок следования чисел последовательности меняется на обратный.
+
 	Входные данные для проверки:
-
-	Для 10 чисел:
-
-	6 7 2 1 120 3 6 672 74 5
-	6 2 1 2 4 6 78 672 19 3
-
-	=> min: 5.
-
-
 
 */
 
 #include <iostream>
-#include <cmath>
 using namespace std;
 
+struct List {
+	int value;
+	List* next;
+	List* prev;
+};
+
+// struct Stack {
+// 	int value;
+// 	List* next;
+// };
+//
+// void pushStack(Stack* &stack; int value) {
+// 	if (stack == nullptr) {
+// 		stack = new Stack();
+// 		stack->value = value;
+// 	} else {
+// 		stack->next = new Stack();
+// 		stack->next->value = value;
+// 		stack = stack->next;
+// 	}
+// }
+//
+// int popStack(Stack* &stack) {
+// 	int lastValue = stack->value;
+// 	stack = stack->next;
+// 	return lastValue;
+// }
+
+
+List* insertItem(List* list, int value) {
+	List* newItem = new List();
+	newItem->value = value;
+	newItem->next = nullptr;
+
+	if (list == nullptr) {
+		newItem->prev = nullptr;
+	} else {
+		newItem->prev = list;
+		newItem->prev->next = newItem;
+	}
+
+	return newItem;
+}
+
+
+List* findFirst(List* list) {
+	while (list->prev != nullptr) list = list->prev;
+	return list;
+}
+
+List* findLast(List* list) {
+	while (list->next != nullptr) list = list->next;
+	return list;
+}
+
+void printList(List* list, int isNegative) {
+	while (list != nullptr) {
+		if (!isNegative && list->value >= 0) {
+			cout << list->value << '\n';
+		}
+
+		if (isNegative && list->value < 0) {
+			cout << list->value << '\n';
+		}
+
+		list = list->prev;
+	}
+}
+
+int oddListCheck(List* list) {
+	while (list->next != nullptr) {
+		if (list->value % 2 == 1 && list->next->value % 2 == 0) {
+			return 1;
+		}
+		list = list->next;
+	}
+
+	return 0;
+}
 
 
 int main()
 {
-
 	setlocale(LC_ALL, "Russian");
 
-	int const COUNT = 30;
-	int x[COUNT], y[COUNT];
+	List* list = nullptr;
+	List* head = nullptr;
+	List* last = nullptr;
+	int count = 0;
 
-	cout << "Введите значения элементов первой последовательности х: ";
-
-	for (int i = 0; i < COUNT; i++) {
-		cin >> x[i];
-	}
-
-	cout << "Введите значения элементов второй последовательности y: ";
-	for (int i = 0; i < COUNT; i++) {
-		cin >> y[i];
-	}
-
-	int min = INT_MAX;
-
-	for (int i = 0, k = 0; i < COUNT; i++) {
-		k = 0;
-		while ((k < COUNT) && (x[i] != y[k])) k++;
-
-		if ((k >= COUNT) && (min > x[i])) {
-			min = x[i];
+	while (count < 2) {
+		cout << "How much items you need? ";
+		cin >> count;
+		if (count < 2) {
+			std::cout << "Hi, please write count more than 1. thanks <3" << '\n';
 		}
 	}
 
-	std::cout << "min: " << min << '\n';
+	int value = 0;
+	while (count--) {
+		cout << "Write new item: ";
+		cin >> value;
+		list = insertItem(list, value);
+	}
+
+	head = findFirst(list);
+	last = findLast(list);
+
+	list = head;
+	int isNegative = !oddListCheck(list);
+
+	list = last;
+	printList(list, isNegative);
+
 	int stop;
 	cin >> stop;
   return 0;
