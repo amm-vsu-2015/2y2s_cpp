9b3aa717c00809bc619a89096b7be75c9848689a
pavbox <pavboxdev@gmail.com>
2017-12-21 04:36:08 +0300

    task3: stack structure as class

--- task3/task3.cpp
+++ task3/task3.cpp
@@ -23,18 +23,19 @@ const int LAB_SIZE = 25;
 
 // base
 struct Position;
-struct Stack;
+class Stack;
 
 void printLabirint(int lab[LAB_SIZE][LAB_SIZE], Position* personAt = nullptr);
 void generateLabirint(int lab[LAB_SIZE][LAB_SIZE]);
 Position* setPerson(int lab[LAB_SIZE][LAB_SIZE]);
 
 // Stack
-Stack* push(Stack* stack, Position* position);
-Stack* pop(Stack* stack);
+// Stack* push(Stack* stack, Position* position);
+// Stack* pop(Stack* stack);
 
 // walking
-Stack* lookAround(int lab[LAB_SIZE][LAB_SIZE], Stack* mainStack, Stack* cell);
+// Stack* lookAround(int lab[LAB_SIZE][LAB_SIZE], Stack* mainStack, Stack* cell);
+Stack* lookAround(int lab[LAB_SIZE][LAB_SIZE], Stack* mainStack, Position* cellPosition);
 void changeCell(int lab[LAB_SIZE][LAB_SIZE], Position* position, int value);
 int checkCell(int lab[LAB_SIZE][LAB_SIZE], Position* cell);
 void walk(int lab[LAB_SIZE][LAB_SIZE], Position* personAt);
@@ -49,6 +50,95 @@ struct Position {
 	}
 };
 
+class Stack {
+private:
+	int count = -1;
+
+public:
+	Position* cells[LAB_SIZE * LAB_SIZE];
+	Stack* next = nullptr;
+
+	void push(Position* position) {
+		cells[++count] = position;
+	}
+
+	Position* pop() {
+		return cells[count--];
+	}
+
+	int isEmpty() {
+		return (count < 0);
+	}
+};
+
+
+void generateLabirint(int lab[LAB_SIZE][LAB_SIZE]) {
+	srand(time(NULL));
+	for (int i = 0; i < LAB_SIZE - 1; i++) {
+		for (int j = 0; j < LAB_SIZE - 1; j++) {
+			lab[i][j] = ((rand() % 100 + 1) % 2 == 0);
+		}
+	}
+}
+
+Position* setPerson(int lab[LAB_SIZE][LAB_SIZE]) {
+	int x, y;
+
+	do {
+		std::cout << "Please, set person position: (x, y) " << '\n';
+
+		std::cin >> x >> y;
+		x--; y--;
+
+		if (lab[x][y]) {
+			std::cout << "Bad position, this is wall. try again..." << '\n';
+		}
+	} while (lab[x][y]);
+
+	return new Position(x, y);
+}
+
+
+
+void walk(int lab[LAB_SIZE][LAB_SIZE], Position* personAt) {
+
+	// Stack* stack = push(nullptr, personAt);
+	Stack* stack = new Stack();
+	stack->push(personAt);
+	changeCell(lab, personAt, 2);
+
+	while(!stack->isEmpty()) {
+		lookAround(lab, stack, stack->pop());
+	}
+}
+
+
+Stack* lookAround(int lab[LAB_SIZE][LAB_SIZE], Stack* mainStack, Position* cellPosition) {
+	// check around of cell
+	for (int idx_x = -1; idx_x < 2; idx_x++) {
+		for (int idx_y = -1; idx_y < 2; idx_y++) {
+			Position* position = new Position(cellPosition->x + idx_x, cellPosition->y + idx_y);
+
+			if (checkCell(lab, position)) {
+				changeCell(lab, position, 2);
+				mainStack->push(position);
+			}
+		}
+	}
+
+	return mainStack;
+}
+
+void changeCell(int lab[LAB_SIZE][LAB_SIZE], Position* position, int value) {
+	lab[position->x][position->y] = value;
+}
+
+int checkCell(int lab[LAB_SIZE][LAB_SIZE], Position* cell) {
+	int isX_correct = (cell->x < LAB_SIZE-1 && cell->x >= 0);
+	int isY_correct = (cell->y < LAB_SIZE-1 && cell->y >= 0);
+
+	return (isX_correct && isY_correct && lab[cell->x][cell->y] == 0);
+}
 
 
 void printLabirint(int lab[LAB_SIZE][LAB_SIZE], Position* personAt) {
@@ -78,100 +168,6 @@ void printLabirint(int lab[LAB_SIZE][LAB_SIZE], Position* personAt) {
 		}
 		std::cout << '\n';
 	}
-}
-
-void generateLabirint(int lab[LAB_SIZE][LAB_SIZE]) {
-	srand(time(NULL));
-	for (int i = 0; i < LAB_SIZE - 1; i++) {
-		for (int j = 0; j < LAB_SIZE - 1; j++) {
-			lab[i][j] = ((rand() % 100 + 1) % 2 == 0);
-		}
-	}
-}
-
-Position* setPerson(int lab[LAB_SIZE][LAB_SIZE]) {
-	int x, y;
-
-	do {
-		std::cout << "Please, set person position: (x, y) " << '\n';
-
-		std::cin >> x >> y;
-		x--; y--;
-
-		if (lab[x][y]) {
-			std::cout << "Bad position, this is wall. try again..." << '\n';
-		}
-	} while (lab[x][y]);
-	std::cout << x << y << '\n';
-	return new Position(x, y);
-}
-
-
-
-struct Stack {
-	Position* position;
-	Stack* next = nullptr;
-};
-
-Stack* push(Stack* stack, Position* position) {
-	Stack* newStack = new Stack();
-	newStack->position = position;
-
-	if (stack != nullptr) {
-		newStack->next = stack;
-	}
-
-	return newStack;
-}
-
-Stack* pop(Stack* stack) {
-	Stack* popped = stack;
-	stack = stack->next;
-
-	return popped;
-}
-
-
-void walk(int lab[LAB_SIZE][LAB_SIZE], Position* personAt) {
-
-	Stack* stack = push(nullptr, personAt);
-	changeCell(lab, personAt, 2);
-
-	while(stack != nullptr) {
-		Stack* popped = stack;
-		stack = stack->next;
-		stack = lookAround(lab, stack, popped);
-	}
-}
-
-
-Stack* lookAround(int lab[LAB_SIZE][LAB_SIZE], Stack* mainStack, Stack* cell) {
-	Position* cellPosition = cell->position;
-
-	// check around of cell
-	for (int idx_x = -1; idx_x < 2; idx_x++) {
-		for (int idx_y = -1; idx_y < 2; idx_y++) {
-			Position* position = new Position(cellPosition->x + idx_x, cellPosition->y + idx_y);
-
-			if (checkCell(lab, position)) {
-				changeCell(lab, position, 2);
-				mainStack = push(mainStack, position);
-			}
-		}
-	}
-
-	return mainStack;
-}
-
-void changeCell(int lab[LAB_SIZE][LAB_SIZE], Position* position, int value) {
-	lab[position->x][position->y] = value;
-}
-
-int checkCell(int lab[LAB_SIZE][LAB_SIZE], Position* cell) {
-	int isX_correct = (cell->x < LAB_SIZE-1 && cell->x >= 0);
-	int isY_correct = (cell->y < LAB_SIZE-1 && cell->y >= 0);
-
-	return (isX_correct && isY_correct && lab[cell->x][cell->y] == 0);
 }
 
 
